2dbf518d6eb6457a6f9911d33c93fcb993f57eb2
samarpan sarkar <samarpansarkar209@gmail.com>
2023-08-06 11:31:38 +0530

    navbar install
    add new navbar form bootstrap.

--- src/App.js
+++ src/App.js
@@ -3,22 +3,24 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React with samarpan
-        </a>
-      </header>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" herf="/">TextUtils</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" herf="/">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" herf="/">Features</a>
+        </li>
+      </ul>
     </div>
+  </div>
+</nav>
   );
 }
 
